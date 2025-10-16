fe137b109f414f7c3af9e9106f0af414ed089a2e
Vinayak Sharma <vinayak@utopiic.com>
2025-10-16 12:40:20 +0530

    update auth-service mongoose

--- services/auth-service/src/index.js
+++ services/auth-service/src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { connectDB } = require('../../shared/db/connection');
+const { connectDB } = require('./db/connection');
 const { init: initRedis } = require('../../shared/redis/redisClient');
 const routes = require('./routes/auth.routes');
 
