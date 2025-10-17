f3b216acb6e74fd10a7b4455232443c99e97b7fc
Vinayak Sharma <vinayak@utopiic.com>
2025-10-17 23:06:11 +0530

    upd conn db

--- services/shared/db/connection.js
+++ services/shared/db/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+uri='mongodb+srv://ashtavinayakcasino:ashtavinayakcasino@cluster0.1xwmd3c.mongodb.net/casino_wallets?retryWrites=true&w=majority&appName=Cluster0'
 
 async function connectDB(uri) {
   try {
