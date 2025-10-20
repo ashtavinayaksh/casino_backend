f594c9d07c6ac7f53274afde3c7873784b851b88
Vinayak Sharma <vinayak@utopiic.com>
2025-10-20 12:35:50 +0530

    update cors parameter for auth and wallet service

--- services/auth-service/src/index.js
+++ services/auth-service/src/index.js
@@ -9,10 +9,12 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://moonbet-casino.vercel.app/",
+      "https://moonbet-casino.vercel.app",
+      "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
