37e72033eb2cdb6384d48717c009d44d44c712c9
Vinayak Sharma <vinayak@utopiic.com>
2025-10-22 17:09:55 +0530

    update db env

--- services/shared/db/connection.js
+++ services/shared/db/connection.js
@@ -1,27 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+uri='mongodb+srv://ashtavinayakcasino:ashtavinayakcasino@cluster0.1xwmd3c.mongodb.net/casino_wallets?retryWrites=true&w=majority&appName=Cluster0'
 
 async function connectDB() {
-  const uri = process.env.MONGO_URI;
-
-  if (!uri) {
-    console.error("❌ MONGO_URI not found in .env file");
-    process.exit(1);
-  }
-
   try {
-    mongoose.set("strictQuery", true);
-    console.log("Connecting to MongoDB...");
+    if (!uri) throw new Error("MongoDB URI is not defined");
 
+    mongoose.set('strictQuery', true);
+
+    console.log("Connecting to MongoDB with URI:", uri);
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
+      dbName: 'casino_wallets',
     });
 
     console.log("✅ MongoDB connected successfully");
     return mongoose.connection;
   } catch (e) {
-    console.error("❌ MongoDB connection error:", e.message);
+    console.error("❌ DB connect error:", e.message);
     process.exit(1);
   }
 }
